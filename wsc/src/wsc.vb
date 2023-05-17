c139d4a3132ee23c92955a42896e19ca9151ac34
GyWare <128908920+gywaredevelopment@users.noreply.github.com>
2023-05-17 18:46:14 +0100

    Add files via upload

--- wsc/src/wsc.vb
+++ wsc/src/wsc.vb
@@ -4,6 +4,12 @@ Imports System.IO
 Module Module1
     Sub Main(args As String())
         On Error GoTo ErrorHandler
+		
+		If Not args.Contains("/nologo", StringComparer.CurrentCultureIgnoreCase) AndAlso Not args.Contains("//nologo", StringComparer.CurrentCultureIgnoreCase) Then
+			Console.WriteLine("GyWare WSCC Windows Script Compiler [https://github.com/gyware/wscc]")
+			Console.WriteLine("Copyright (C) GyWare. All rights reserved.")
+			Console.WriteLine()
+		End If
 
         Dim argTarget As String = GetArgumentValue(args, "target")
         If argTarget = "" Then
@@ -12,11 +18,13 @@ Module Module1
 
         Dim argEngine As String = GetArgumentValue(args, "engine")
 
-        Call compileScript(args(0), argTarget, argEngine)
+		Dim argPath As String = args(0)
+
+        Call compileScript(argPath, argTarget, argEngine)
 
 ErrorHandler:
         If Err.Number <> 0 Then
-            Console.WriteLine("Error #" & Str(Err.Number) & ": " & Err.Description)
+            Console.WriteLine("Error " & CStr(Err.Number) & ": " & Err.Description)
         End If
     End Sub
 
@@ -27,29 +35,44 @@ ErrorHandler:
         Dim jsPath As String = Path.Combine(tempDir, Path.GetRandomFileName())
 
         Dim output As String
+		Dim exeTarget As String
 
-        If scriptEngine <> "" Then
-            output = "import System;import System.IO;import System.Diagnostics;var args:String[]=Environment.GetCommandLineArgs();var c='" & Replace(Replace(Replace(Replace(Replace(Replace(Replace(Replace(scriptContent, "\r", "\\r"), "\n", "\\n"), "\r\n", "\\r\\n"), "'", "\'"), vbCr, "\r"), vbLf, "\n"), vbCrLf, "\r\n"), "\\", "\\\\") & "',f=Path.GetTempFileName();File.WriteAllText(f,c);var p=Process.Start('" & scriptTarget & "','//nologo //e:" & scriptEngine & " '+f+' '+args.join(' '));p.WaitForExit();File.Delete(f);"
+		If Not scriptContent.ToLower().Contains("wscript") AndAlso Not scriptContent.ToLower().Contains("wsh") AndAlso scriptEngine.ToLower() = "jscript" Then
+		output = scriptContent
+		If scriptTarget.ToLower() = "cscript" Then
+			exeTarget = "exe"
+		Else
+			exeTarget = "winexe"
+		End If
         Else
-            output = "import System;import System.IO;import System.Diagnostics;var args:String[]=Environment.GetCommandLineArgs();var c='" & Replace(Replace(Replace(Replace(Replace(Replace(Replace(Replace(scriptContent, "\r", "\\r"), "\n", "\\n"), "\r\n", "\\r\\n"), "'", "\'"), vbCr, "\r"), vbLf, "\n"), vbCrLf, "\r\n"), "\\", "\\\\") & "',f=Path.ChangeExtension(Path.Combine(Path.GetTempPath(),Path.GetRandomFileName()), '" & Path.GetExtension(scriptPath) & "');File.WriteAllText(f,c);var p=Process.Start('" & scriptTarget & "','//nologo '+f+' '+args.join(' '));p.WaitForExit();File.Delete(f);"
+		If scriptEngine <> "" Then
+			output = "import System;import System.IO;import System.Diagnostics;var args:String[]=Environment.GetCommandLineArgs();var c='" & Replace(Replace(Replace(Replace(Replace(Replace(Replace(Replace(scriptContent, "\r", "\\r"), "\n", "\\n"), "\r\n", "\\r\\n"), "'", "\'"), vbCr, "\r"), vbLf, "\n"), vbCrLf, "\r\n"), "\\", "\\\\") & "',f=Path.ChangeExtension(Path.Combine(Path.GetTempPath(),Path.GetRandomFileName()), '" & Path.GetExtension(scriptPath) & "');File.WriteAllText(f,c);var p=Process.Start('" & scriptTarget & "','//nologo //e:" & scriptEngine & " '+f+' '+args.join(' '));p.WaitForExit();File.Delete(f);"
+        Else
+			output = "import System;import System.IO;import System.Diagnostics;var args:String[]=Environment.GetCommandLineArgs();var c='" & Replace(Replace(Replace(Replace(Replace(Replace(Replace(Replace(scriptContent, "\r", "\\r"), "\n", "\\n"), "\r\n", "\\r\\n"), "'", "\'"), vbCr, "\r"), vbLf, "\n"), vbCrLf, "\r\n"), "\\", "\\\\") & "',f=Path.ChangeExtension(Path.Combine(Path.GetTempPath(),Path.GetRandomFileName()), '" & Path.GetExtension(scriptPath) & "');File.WriteAllText(f,c);var p=Process.Start('" & scriptTarget & "','//nologo '+f+' '+args.join(' '));p.WaitForExit();File.Delete(f);"
         End If
+		End If
 
         File.WriteAllText(jsPath, output)
 
         Dim args As String() = Environment.GetCommandLineArgs
         Dim out As String = GetArgumentValue(args, "out")
         If out = "" Then
-            out = Path.Combine(Path.GetDirectoryName(scriptPath), Path.GetFileNameWithoutExtension(scriptPath) & ".exe")
+            out = Path.ChangeExtension(Path.GetFullPath(scriptPath), ".exe")
         Else
             out = Path.GetFullPath(out)
         End If
 
-		Dim jsc = New Process()
-		jsc.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Microsoft.NET", "Framework64", "v4.0.30319", "jsc.exe")
-		jsc.StartInfo.Arguments = "/nologo /target:winexe /out:" & out & " " & jsPath
-		jsc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
-        jsc.Start()
+		If Not GetArgumentValue(args, "autocompile").ToLower() = "false" Then
+		Dim jscInfo = New ProcessStartInfo()
+		jscInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Microsoft.NET", "Framework64", "v4.0.30319", "jsc.exe")
+		jscInfo.Arguments = "/nologo /target:winexe /out:""" & out & """ " & jsPath
+		jscInfo.CreateNoWindow = True
+		jscInfo.UseShellExecute = False
+        Dim jsc = Process.Start(jscInfo)
 		jsc.WaitForExit()
+		Else
+		File.WriteAllText(Path.ChangeExtension(out, ".js"), output)
+		End If
 		
         File.Delete(jsPath)
         Return ""
@@ -60,7 +83,7 @@ ErrorHandler:
             If arg.StartsWith("/") AndAlso arg.Contains(":"c) Then
                 Dim argParts As String() = arg.Substring(1).Split(":")
                 If argParts(0).ToLower() = argName.ToLower() Then
-                    Return argParts(1)
+                    Return Replace(argParts(1), """", "")
                 End If
             End If
         Next
